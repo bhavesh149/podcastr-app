b6c30e928e9351abf79ce9cfcd2e6778ba5b7d02
Bhavesh Mahajan <101736101+bhavesh149@users.noreply.github.com>
2024-06-15 00:27:59 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://unbiased-wolf-80.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
